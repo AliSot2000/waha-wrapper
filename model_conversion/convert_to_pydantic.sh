5c489c707d44385edfcd8f05ca012ae5eba9cfc1
AliSot2000 <alisot200@gmail.com>
2024-07-01 20:52:03 +0200

    + Customizations

--- model_conversion/convert_to_pydantic.sh
+++ model_conversion/convert_to_pydantic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-datamodel-codegen --input openapi_formatted.json --input-file-type openapi --output waha-model.py
+datamodel-codegen --use-union-operator --allow-population-by-field-name --input openapi_formatted.json --input-file-type openapi --output waha-model.py
