d5d73d1978686eadc30f804b5df5c78a1bf5164c
AliSot2000 <alisot200@gmail.com>
2024-07-02 01:00:30 +0200

    + Created package for api endpoints + Created first points for some endpoints

---
+++ src/waha_python_wrapper/api/session.py
@@ -0,0 +1,48 @@
+from waha_python_wrapper.tools import api_endpoint_wrapper, Methods
+import waha_python_wrapper.waha_model as wm
+
+start_session = api_endpoint_wrapper(path="/api/sessions/start",
+                                     request_model=wm.SessionStartRequest,
+                                     response_model=wm.SessionDTO,
+                                     expected_code=201,
+                                     body_defaults={"name": "default"},
+                                     method=Methods.POST,
+                                     docstring="""
+                                     Async python wrapper for the WAHA API to start a session
+                                     
+                                     session.name: str = "default"
+                                     
+                                     :param req: SessionStartRequest
+                                     :return: SessionDTO
+                                     """
+                                     )
+
+
+stop_session = api_endpoint_wrapper(path="/api/sessions/stop",
+                                    request_model=wm.SessionStopRequest,
+                                    expected_code=201,
+                                    body_defaults={"name": "default"},
+                                    method=Methods.POST,
+                                    docstring="""
+                                     Async python wrapper for the WAHA API to stop a session
+
+                                     session.name: str = "default"
+
+                                     :param req: SessionStartRequest
+                                     :return: SessionDTO
+                                     """
+                                    )
+
+
+list_session = api_endpoint_wrapper(path="/api/sessions/",
+                                    response_model=wm.SessionListDTO,
+                                    expected_code=200,
+                                    method=Methods.GET,
+                                    docstring="""
+                                     Async python wrapper for the WAHA API to list sessions
+
+                                     :return: SessionListDTO
+                                     """
+                                    )
+
+
